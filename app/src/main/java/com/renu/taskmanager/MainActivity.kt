f4f4984a35eb1b91b8032eadb0ce4eb3b9aeacbe
renuswami <renuswami2001@gmail.com>
2024-01-22 22:26:47 +0530

    All Task Completed

--- app/src/main/java/com/renu/taskmanager/MainActivity.kt
+++ app/src/main/java/com/renu/taskmanager/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -42,18 +44,27 @@ class MainActivity : ComponentActivity() {
 fun Greeting(massage1: String, massages2: String, modifier: Modifier = Modifier) {
 
     val image = painterResource(R.drawable.ic_task_completed)
-    Box {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Column(
+            verticalArrangement = Arrangement.Center ,
+            horizontalAlignment =  Alignment.CenterHorizontally
+        ) {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+
         )
-        Column {
+
 
             Text(
                 text = massage1,
                 fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                modifier = modifier
+                lineHeight = 116.sp,
+                        modifier = modifier
                     .padding(
                         top = 24.dp,
                         bottom = 8.dp
@@ -62,7 +73,8 @@ fun Greeting(massage1: String, massages2: String, modifier: Modifier = Modifier)
             Text(
                 text = massages2,
                 fontSize = 16.sp,
-                textAlign = TextAlign.Center
+                lineHeight = 116.sp
+
             )
         }
 
